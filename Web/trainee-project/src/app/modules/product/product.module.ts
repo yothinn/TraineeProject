de0d415ad18e6bbef91b0436629fdfcaa7e4abe1
ThanachaiBN <nine3333fox@gmail.com>
2021-07-16 15:37:46 +0700

    testt

--- Web/trainee-project/src/app/modules/product/product.module.ts
+++ Web/trainee-project/src/app/modules/product/product.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './product.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [                        ///
+  {                                       
+    path:'',
+    component:ProductComponent
+  }
 
+]                                              ////
 
 @NgModule({
   declarations: [ProductComponent],
-  imports: [
+  imports: [ RouterModule.forChild(routes),     //////
     CommonModule
   ]
 })
