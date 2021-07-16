706624af46597c16fda4f33d7901aa1105d6d2f1
BlankerGamer <Bankone55@gmail.com>
2021-07-16 14:29:23 +0700

    router

--- Web/trainee-project/src/app/modules/attendance/attendance.module.ts
+++ Web/trainee-project/src/app/modules/attendance/attendance.module.ts
@@ -1,13 +1,20 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AttendanceComponent } from './attendance.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  {
+    path:'',
+    component:AttendanceComponent
+  }
+]
 
 @NgModule({
   declarations: [AttendanceComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class AttendanceModule { }
