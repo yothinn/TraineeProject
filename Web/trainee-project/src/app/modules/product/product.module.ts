05e129bcb27dc803dec9d5f54ca5f226998f0c1d
siralukkuamwong <siraluk.kua@northbkk.ac.th>
2021-07-16 16:22:04 +0700

    'product-dialog-details'

--- Web/trainee-project/src/app/modules/product/product.module.ts
+++ Web/trainee-project/src/app/modules/product/product.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './product.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductDialogDetailsComponent } from './product-dialog-details/product-dialog-details.component';
 
 const routes: Routes = [{
   path: '',
@@ -9,7 +10,7 @@ const routes: Routes = [{
 }]
 
 @NgModule({
-  declarations: [ProductComponent],
+  declarations: [ProductComponent, ProductDialogDetailsComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes)
