56933423ef9b1f61c751632e21d8a47dad8ab98b
siralukkuamwong <siraluk.kua@northbkk.ac.th>
2021-07-16 15:24:18 +0700

    'product-module'

--- Web/trainee-project/src/app/modules/product/product.module.ts
+++ Web/trainee-project/src/app/modules/product/product.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './product.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [{
+  path: '',
+  component: ProductComponent
+}]
 
 @NgModule({
   declarations: [ProductComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class ProductModule { }
