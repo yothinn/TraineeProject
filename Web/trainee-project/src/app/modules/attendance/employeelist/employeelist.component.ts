b4eecfa9d3047aaf4ccfe5eebfe4e7bcab00f6b7
BlankerGamer <Bankone55@gmail.com>
2021-07-19 15:51:32 +0700

    commit

--- Web/trainee-project/src/app/modules/attendance/employeelist/employeelist.component.ts
+++ Web/trainee-project/src/app/modules/attendance/employeelist/employeelist.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AttendanceService } from '../attendance.service';
+
 
 @Component({
   selector: 'app-employeelist',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeelistComponent implements OnInit {
 
-  constructor() { }
+  employeeData: any;
+
+  constructor(private attendanceService: AttendanceService) { }
 
   ngOnInit(): void {
+    this.attendanceService.getAttendance().subscribe((res:any)=>{
+      console.log(res)
+      this.employeeData = res.data
+      console.log(this.employeeData)
+    })
   }
 
 }
