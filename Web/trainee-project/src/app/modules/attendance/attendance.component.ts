e9fab8f3117fcb641efef774d38e557e0e77360b
BlankerGamer <Bankone55@gmail.com>
2021-07-19 11:17:15 +0700

    genproject

--- Web/trainee-project/src/app/modules/attendance/attendance.component.ts
+++ Web/trainee-project/src/app/modules/attendance/attendance.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-attendance',
   templateUrl: './attendance.component.html',
   styleUrls: ['./attendance.component.scss']
 })
 export class AttendanceComponent implements OnInit {
-
+ 
   constructor() { }
 
   ngOnInit(): void {
