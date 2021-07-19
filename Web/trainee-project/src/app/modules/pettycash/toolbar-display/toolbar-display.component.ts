281120e994dce8234731aa3f3fdc80ad16becc0f
baihakee <baihaki3851@gmail.com>
2021-07-19 14:59:47 +0700

    pull

---
+++ Web/trainee-project/src/app/modules/pettycash/toolbar-display/toolbar-display.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar-display',
+  templateUrl: './toolbar-display.component.html',
+  styleUrls: ['./toolbar-display.component.scss']
+})
+export class ToolbarDisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
