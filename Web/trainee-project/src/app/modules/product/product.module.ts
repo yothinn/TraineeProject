f782545f9b5c49a6ca567b710a7c3001dbceef2d
siralukkuamwong <siraluk.kua@northbkk.ac.th>
2021-07-19 17:16:39 +0700

    details

--- Web/trainee-project/src/app/modules/product/product.module.ts
+++ Web/trainee-project/src/app/modules/product/product.module.ts
@@ -3,16 +3,18 @@ import { CommonModule } from '@angular/common';
 import { ProductComponent } from './product.component';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductDialogDetailsComponent } from './product-dialog-details/product-dialog-details.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 const routes: Routes = [{
   path: '',
-  component: ProductComponent
+  component: ProductDialogDetailsComponent
 }]
 
 @NgModule({
   declarations: [ProductComponent, ProductDialogDetailsComponent],
   imports: [
     CommonModule,
+    SharedModule,
     RouterModule.forChild(routes)
   ]
 })
