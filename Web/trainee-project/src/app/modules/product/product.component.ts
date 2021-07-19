9afbc27072e987acfcf78780beac5ba8fd7d9835
ThanachaiBN <nine3333fox@gmail.com>
2021-07-19 14:45:24 +0700

    testtt

--- Web/trainee-project/src/app/modules/product/product.component.ts
+++ Web/trainee-project/src/app/modules/product/product.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
@@ -7,6 +12,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+    ,
+    {value: 'tacos-2', viewValue: 'Tacos'}
+    ,
+    {value: 'tacos-2', viewValue: 'Tacos'}
+    ,
+    {value: 'tacos-2', viewValue: 'Tacos'}
+    ,
+    {value: 'tacos-2', viewValue: 'Tacos'}
+    ,
+    {value: 'tacos-2', viewValue: 'Tacos'}
+    ,
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ]; 
   constructor() { }
 
   ngOnInit(): void {
--- Web/trainee-project/src/app/modules/product/product.module.ts
+++ Web/trainee-project/src/app/modules/product/product.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './product.component';
 import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 const routes: Routes = [{
   path: '',
@@ -12,6 +13,7 @@ const routes: Routes = [{
   declarations: [ProductComponent],
   imports: [
     CommonModule,
+    SharedModule,
     RouterModule.forChild(routes)
   ]
 })
