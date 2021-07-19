3ea12f7339aa8f54b103d0a20481ba31fce837a2
ThanachaiBN <nine3333fox@gmail.com>
2021-07-19 14:46:56 +0700

    category-module

---
+++ Server/trainee-project/src/modules/categories/models/model.js
@@ -0,0 +1,56 @@
+'use strict';
+// use model
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var CategoriesSchema = new Schema({
+    name: {
+        type: String,
+        required: 'Please fill a Categories name',
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    updated: {
+        type: Date
+    },
+    createby: {
+        _id: {
+            type: String
+        },
+        username: {
+            type: String
+        },
+        displayname: {
+            type: String
+        }
+    },
+    updateby: {
+        _id: {
+            type: String
+        },
+        username: {
+            type: String
+        },
+        displayname: {
+            type: String
+        }
+    }
+});
+CategoriesSchema.pre('save', function(next){
+    let Categories = this;
+    const model = mongoose.model("Categories", CategoriesSchema);
+    if (Categories.isNew) {
+        // create
+        next();
+    }else{
+        // update
+        Categories.updated = new Date();
+        next();
+    }
+    
+    
+})
+mongoose.model("Categories", CategoriesSchema);
