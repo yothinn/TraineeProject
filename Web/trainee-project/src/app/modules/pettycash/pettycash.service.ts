ebabb8cb6c513a2e25b8e660b70a69ea29401b03
baihakee <baihaki3851@gmail.com>
2021-07-20 20:12:17 +0700

    pull

--- Web/trainee-project/src/app/modules/pettycash/pettycash.service.ts
+++ Web/trainee-project/src/app/modules/pettycash/pettycash.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PettycashService {
+  customer:any;
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+
+  createCustomer(customerList : any){
+  
+  }
 }
