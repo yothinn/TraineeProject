8a1da0adeaa88583d6a47cea8ac5f60bd9ea05cd
Golfsupakorn <supakorn.num@northbkk.ac.th>
2021-07-21 16:27:31 +0700

    commit

--- Web/trainee-project/src/app/modules/attendance/dialog-add/dialog-add.component.ts
+++ Web/trainee-project/src/app/modules/attendance/dialog-add/dialog-add.component.ts
@@ -3,22 +3,22 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { AttendanceService } from '../attendance.service';
 
+
 @Component({
   selector: 'app-dialog-add',
   templateUrl: './dialog-add.component.html',
   styleUrls: ['./dialog-add.component.scss']
 })
 export class DialogAddComponent implements OnInit {
-  userForm:FormGroup;
+  userForm: FormGroup;
   
-  
-
   constructor(
     private attendanceService: AttendanceService,
     private fb: FormBuilder,
     public dialogRef: MatDialogRef<DialogAddComponent>,
-    @Inject(MAT_DIALOG_DATA) public data
-    ) { }
+    @Inject(MAT_DIALOG_DATA) public data,
+
+  ) { }
 
   ngOnInit(): void {
     // console.log(this.data)
@@ -38,15 +38,19 @@ export class DialogAddComponent implements OnInit {
       tel: [data.tel]
     })
   }
-  
-  Onsubmit(){
+
+  Onsubmit() {
     // console.log(this.userForm.value)
-      this.attendanceService.createattendan(this.userForm.value)
-        .subscribe(res => {
-          if (res) {
-            this.dialogRef.close(res);
-          }
-        })
-      }
+    this.attendanceService.createattendan(this.userForm.value)
+      .subscribe(res => {
+        if (res) {
+          this.dialogRef.close(res);
+        }
+      })
+  }
+  backClicked() {
+    this.dialogRef.close();
+  }
+
 }
 
