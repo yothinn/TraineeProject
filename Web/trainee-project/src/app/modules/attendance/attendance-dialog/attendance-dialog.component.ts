dc09b6d86ef1074c3b11eb6ce83f3f1ed7bcf086
BlankerGamer <Bankone55@gmail.com>
2021-07-30 15:24:54 +0700

    commit

--- Web/trainee-project/src/app/modules/attendance/attendance-dialog/attendance-dialog.component.ts
+++ Web/trainee-project/src/app/modules/attendance/attendance-dialog/attendance-dialog.component.ts
@@ -1,6 +1,10 @@
+import { Inject } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { FormGroup } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { AttendanceService } from '../attendance.service';
+import { DialogAddComponent } from '../dialog-add/dialog-add.component';
 
 @Component({
   selector: 'app-attendance-dialog',
@@ -12,16 +16,19 @@ export class AttendanceDialogComponent implements OnInit {
   workDateForm: FormGroup;
   employeeData: any;
   selected = 'option2';
-  
-  constructor(private attendanceService: AttendanceService) { }
+
+
+  constructor(private attendanceService: AttendanceService,
+    private fb: FormBuilder,
+    public dialogRef: MatDialogRef<DialogAddComponent>,
+    @Inject(MAT_DIALOG_DATA) public data,) { }
 
   ngOnInit(): void {
 
-    this.attendanceService.getAttendance().subscribe((res:any)=>{
+    this.attendanceService.getAttendance().subscribe((res: any) => {
       this.employeeData = res.data;
-     });
+    });
   }
 
 
-  
 }
