d8292991c7bf1f9dc0448ef2b05089236f73787c
ThanachaiBN <nine3333fox@gmail.com>
2021-08-09 15:41:35 +0700

    ddd

--- Web/trainee-project/src/app/modules/product/product.service.ts
+++ Web/trainee-project/src/app/modules/product/product.service.ts
@@ -45,4 +45,12 @@ export class ProductService {
     return this.http.delete(`http://localhost:3000/api/categories/${body._id}`, body);
   }
 
+  //getTest(body:any):Observable<any> {
+    // return this.http.get('http://localhost:3000/api/products', body);
+    // return this.http.post(`http://localhost:3000/api/products/${body._id}`, body);
+    // return this.http.get('http://localhost:3000/api/products?body._id&body.productName');
+  //}
+  
+ 
+
 }
