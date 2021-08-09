5f8a6c2214b01203765c15a9333784d7b1c19586
kriangkrai <kriangkrai86@gmail.com>
2021-08-09 20:10:32 +0700

    .

--- Server/trainee-project/src/modules/products/controllers/controller.js
+++ Server/trainee-project/src/modules/products/controllers/controller.js
@@ -123,19 +123,12 @@ exports.delete = function (req, res) {
 exports.search = function (req, res) {
     let searchText = req.query.query;
     let query = {
-
         productName: { $regex: `${searchText}`}
         // $or: [
         //     { productName: { $regex: `^${searchText}`, $options: "i" } }
         //     { lastName: { $regex: `^${searchText}`, $options: "i" } }
         // ]
     };
-    console.log(query);
-
-    // console.log(searchText)
-    // console.log(req.query.query);
-    // console.log(query);
-
     Products.find(query, function (err, datas) {
         if (err) {
             return res.status(400).send({
